e713db799f901591e4f5f4888295f0d73d829d00
James.K.Crook <james.k.crook@gmail.com>
2014-10-24 22:34:47 +0000

    Possible fix for position of OnPreferences Dialog.

--- audacity-src/trunk/src/widgets/Meter.cpp
+++ audacity-src/trunk/src/widgets/Meter.cpp
@@ -1537,6 +1537,8 @@ void Meter::OnPreferences(wxCommandEvent & WXUNUSED(event))
    dlg.Layout();
    dlg.Fit();
 
+   dlg.CenterOnParent();
+
    if (dlg.ShowModal() == wxID_OK)
    {
       gPrefs->Write(wxT("/Meter/MeterRefreshRate"), mMeterRefreshRate);
