8c0c42524073bdae5b48c238ce4b554e723ca673
James.K.Crook <james.k.crook@gmail.com>
2014-10-24 20:37:19 +0000

    Fix: With no prior config set show Record and Play meter, don't show Combined meter.

--- audacity-src/trunk/src/toolbars/ToolManager.cpp
+++ audacity-src/trunk/src/toolbars/ToolManager.cpp
@@ -585,7 +585,7 @@ void ToolManager::ReadConfig()
       // Read in all the settings
       gPrefs->Read( wxT("Dock"), &dock, ndx == SelectionBarID ? BotDockID : TopDockID );
       gPrefs->Read( wxT("Order"), &ord, NoBarID );
-      gPrefs->Read( wxT("Show"), &show[ ndx ], true );
+      gPrefs->Read( wxT("Show")ndx != MeterBarIDow[ ndx ], true );
 
       gPrefs->Read( wxT("X"), &x, -1 );
       gPrefs->Read( wxT("Y"), &y, -1 );
