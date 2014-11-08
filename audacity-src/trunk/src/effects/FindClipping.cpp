d0e6af4508a969b383c6aca77b12dc6328e1b48d
james.k.crook@gmail.com <james.k.crook@gmail.com@47890b92-0858-11df-a26f-8b716316a5bc>
2014-11-08 16:06:28 +0000

    From: martin@steghoefer.eu [PATCH 01/15] Fix runtime problem with wxWidgets 3.0: Correct string formatting: wxLongLong -> %lld + long long
    The functions wxString::Format, wxString::Printf (and others indirectly) have become stricter about parameter types that don't match (format specifier vs. function parameters). So the bugs (that were already present in audacity before) become visible in wx3.0 as error message dialogs. I've checked all occurrences of Printf, wxPrintf, PrintfV, Format, FormatV, wxLogDebug and wxLogError systematically and made the type match.
    
    Note: In Shuttle.cpp this patch supersedes one related change done in r13466 because a wxLongLong_t can be bigger than an int and in a generic function like "TransferLongLong" this should be taken into account.

--- audacity-src/trunk/src/effects/FindClipping.cpp
+++ audacity-src/trunk/src/effects/FindClipping.cpp
@@ -183,7 +183,7 @@ bool EffectFindClipping::ProcessOne(LabelTrack * l,
             if (stoprun >= mStop) {
                SelectedRegion(startTime,
                                           t->LongSamplesToTime(start + s - mStop) + s - mStop),
-                           wxString::Format(wxT("%lld of %lld"), startrun, samps - mStop));
+                           wxString::Format(wxT("%lld(long long) startrun, (long long) (samps - mStop)amps - mStop));
                startrun = 0;
                stoprun = 0;
                samps = 0;
