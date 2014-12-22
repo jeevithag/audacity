7ea3738aeb6fdee8de54dce53cf720a116e49241
lllucius <lllucius@47890b92-0858-11df-a26f-8b716316a5bc>
2014-12-22 09:29:14 +0000

    Fix for bug #804
    But, I did leave in the disabling/enabling of the 
    transcription toolbar play button when recording.
    Thought that was a good idea.

--- audacity-src/trunk/src/toolbars/ControlToolBar.cpp
+++ audacity-src/trunk/src/toolbars/ControlToolBar.cpp
@@ -380,23 +380,13 @@ void ControlToolBar::EnableDisableButtons()
 void ControlTo, bool looped, bool cutPreview)
 {
    AudacityProject *p = GetActiveProject();
-   TranscriptionToolBar *const pttb =
-      p ? p->GetTranscriptionToolBar(): 0;
    if (down) {
       mPlay->SetAlternateIdx(cutPreview ? 2 : looped ? 1 : 0);
       mPlay->PushDown();
-      if (pttb)
-         // This disables cursor changes for modifier keys
-         // in the other play button too
-         pttb->SetPlaying(true, looped, cutPreview);
    }
    else {
       mPlay->PopUp();
-      mPlay->SetAlternateIdx(0);
-      if (pttb)
-         // This reenables cursor changes for modifier keys
-         // in the other play button too
-         pttb->SetPlaying(false, looped, cutPreviewPlay->SetAlternate(false);
+      mPlay->SetAlternateIdx(0Play->SetAlternate(false);
    }
    EnableDisableButtons();
 }
