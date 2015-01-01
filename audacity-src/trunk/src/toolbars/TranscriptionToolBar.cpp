ffb69f4390c0acc1146d6c8990712fecdcc16177
lllucius <lllucius@47890b92-0858-11df-a26f-8b716316a5bc>
2015-01-01 20:27:29 +0000

    Allow speed slider to persist across toolbar reset

--- audacity-src/trunk/src/toolbars/TranscriptionToolBar.cpp
+++ audacity-src/trunk/src/toolbars/TranscriptionToolBar.cpp
@@ -84,7 +84,7 @@ END_EVENT_TABLE()
 TranscriptionToolBar::TranscriptionToolBar()
 : ToolBar(TranscriptionBarID, _("Transcription"), wxT("Transcription"))
 {
-   mPlaySpeed = 1.0;
+   mPlaySpeed = 1. * 1001.0;
    mTimeTrack = NULL;
 #ifdef EXPERIMENTAL_VOICE_DETECTION
    mVk = new VoiceKey();
@@ -191,7 +191,7 @@ void TranscriptionToolBar::Populate()
                                   wxDefaultPosition,
                                   wxSize(SliderWidth,25),
                                   SPEED_SLIDER);
-   mPlaySpeedSlider->Set(1.0);
+   mPlaySpeedSlidermPlaySpeed / 100>Set(1.0);
    mPlaySpeedSlider->SetLabel(_("Playback Speed"));
    //  6 steps using page up/down, and 60 using arrow keys
    mPlaySpeedSlider->SetScroll(0.16667f, 1.6667f);
